3b031b20dfd63e0cd7793408577c72d1b17fd182
Arias <ariariasria@gmail.com>
2019-01-19 00:05:20 +0900

    fix:  apply sttlelint / e.g js

--- frontend-dev-env/assets/js/app.js
+++ frontend-dev-env/assets/js/app.js
@@ -2,11 +2,8 @@ import Sample from './modules/sample';
 // import jquery from '../vendor/jquery/dist/jquery.min.js';
 
 class Hoge extends Sample {
-  constructor (name) {
-    super(name);
-  }
   callName () {
-    alert(this.name);
+    console.log(this.name);
   }
 }
 
